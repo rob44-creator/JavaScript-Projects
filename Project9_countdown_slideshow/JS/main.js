1b9c1bde20a0c84149006832cc053e203e5aa5e3
rob44-creator <numeroone51@hotmail.com>
2026-01-29 13:04:29 +0000

    Add files via upload

---
+++ Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,56 @@
+//main.js
+
+//Countdown timer function
+function countdown() {
+
+  //input from user
+  var seconds = document.getElementById("seconds").value;
+
+  //function to start countdown
+  function tick() {
+    seconds = seconds - 1;
+    timer.innerHTML = seconds;
+
+    if (seconds > 0) {
+      setTimeout(tick, 1000);
+    } else {
+      alert("Time's up!");
+    }
+}
+tick();
+}
+
+//Image slideshow functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+//thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
